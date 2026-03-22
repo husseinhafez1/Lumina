441ef09f2b845350c3c5c9af83edce52b436f4de
husseinhafez1 <husseinhafez690@gmail.com>
2026-03-22 18:21:30 +0100

    abstracting vertex buffer

---
+++ Lumina/src/VertexBuffer.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include <glad/glad.h>
+
+class VertexBuffer {
+public:
+	unsigned int ID;
+
+	VertexBuffer(const void* data, unsigned int size) {
+		glGenBuffers(1, &ID);
+		glBindBuffer(GL_ARRAY_BUFFER, ID);
+		glBufferData(GL_ARRAY_BUFFER, size, data, GL_STATIC_DRAW);
+	}
+
+	~VertexBuffer() {
+		glDeleteBuffers(1, &ID);
+	}
+
+	void Bind() const {
+		glBindBuffer(GL_ARRAY_BUFFER, ID);
+	}
+
+	void Unbind() const {
+		glBindBuffer(GL_ARRAY_BUFFER, 0);
+	}
+
+	VertexBuffer(const VertexBuffer&) = delete;
+	VertexBuffer& operator=(const VertexBuffer&) = delete;
+};
--- Lumina/src/main.cpp
+++ Lumina/src/main.cpp
@@ -9,6 +9,8 @@
 
 #include "shader.h"
 #include "Camera.h"
+#include "VertexBufferLayout.h"
+#include "VertexBuffer.h"
 
 
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
@@ -106,6 +108,12 @@ int main() {
 		0, 3, 2 
 	};
 
+	VertexBufferLayout layout;
+	layout.push<float>(3);
+	layout.push<float>(2);
+
+	VertexBuffer vbo(vertices, sizeof(vertices));
+
 	unsigned int texture1, texture2;
 	glGenTextures(1, &texture1);
 	glActiveTexture(GL_TEXTURE0);
@@ -149,13 +157,10 @@ int main() {
 	}
 	stbi_image_free(data);
 
-	unsigned int VAO, VBO, EBO;
+	unsigned int VAO, EBO;
 	glGenVertexArrays(1, &VAO);
 	glBindVertexArray(VAO);
-	glGenBuffers(1, &VBO);
 	glGenBuffers(1, &EBO);
-	glBindBuffer(GL_ARRAY_BUFFER, VBO);
-	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
 	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 5 * sizeof(float), (void*)0);
