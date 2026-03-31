e455302c10a6fce9a2b2e36c6a82db6c3290fdd1
husseinhafez1 <husseinhafez690@gmail.com>
2026-03-31 16:55:54 +0200

    Point lights

--- Lumina/res/color.fs
+++ Lumina/res/color.fs
@@ -8,11 +8,15 @@ struct Material {
 }; 
 
 struct Light {
-    vec3 direction;
+    vec3 position;
 
     vec3 ambient;
     vec3 diffuse;
     vec3 specular;
+
+    float constant;
+    float linear;
+    float quadratic;
 };
 
 in vec3 FragPos;  
@@ -25,21 +29,27 @@ uniform Light light;
 
 void main()
 {
-    vec3 lightDir = normalize(-light.direction);
+    float distance = length(light.position - FragPos);
+    float attenuation = 1.0 / (light.constant + light.linear * distance + 
+                light.quadratic * (distance * distance));
 
     // ambient
     vec3 ambient = light.ambient * texture(material.diffuse, TexCoords).rgb;
+    ambient*=attenuation;
   	
     // diffuse 
     vec3 norm = normalize(Normal);
+    vec3 lightDir = normalize(light.position - FragPos);
     float diff = max(dot(norm, lightDir), 0.0);
-    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;  
+    vec3 diffuse = light.diffuse * diff * texture(material.diffuse, TexCoords).rgb;
+    diffuse*=attenuation;
     
     // specular
     vec3 viewDir = normalize(viewPos - FragPos);
     vec3 reflectDir = reflect(-lightDir, norm);  
     float spec = pow(max(dot(viewDir, reflectDir), 0.0), material.shininess);
     vec3 specular = light.specular * spec * vec3(texture(material.specular, TexCoords));
+    specular*=attenuation;
         
     vec3 result = ambient + diffuse + specular;
     FragColor = vec4(result, 1.0);
